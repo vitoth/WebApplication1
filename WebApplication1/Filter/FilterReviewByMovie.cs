45c2268767ac68c6a58d6a30090b432397421094
vitoth <viktor965@gmail.com>
2016-04-23 18:06:21 +0200

    Added FilterReviewByMovie

---
+++ WebApplication1/Filter/FilterReviewByMovie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Portal
+{
+    public class FilterReviewByMovie : IReviewFilter
+    {
+        public FilterReviewByMovie(Movie movie)
+        {
+            this.movie = movie;
+        }
+
+        public bool Pass(Review review)
+        {
+            return review.movie == this.movie;
+        }
+
+        private Movie movie;
+    }
+}
